3539bb18b22a23103ae9a1fc4e83df27b90ca681
jonah <mudzingwajona@gmail.com>
2023-09-29 19:54:41 +0200

    fix index.css not found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-400">
           Hello world
     </div>
   );
