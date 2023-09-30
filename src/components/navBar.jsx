d5a6a9dc902e96ab279ea2ca09f05399de1378dd
jonah <mudzingwajona@gmail.com>
2023-09-30 13:51:19 +0200

    features (navbar) add more html element to the nav bar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,27 @@
 
 const NavBar = ()=>{
     return (
-        <nav className="bg-teal-500 m-sm text-center"> hello</nav>
+        <div className="bg-gray-800 text-white flex justify-between p-2">
+            <div className="text-xl ">Wire</div>
+            <ul className="flex justify-between gap-4">
+                <li className="hover:border-b-2 border-b-purple-700">Home</li>
+                <li className="hover:border-b-2 border-b-purple-700">About</li>
+                <li className="hover:border-b-2 border-b-purple-700">Services</li>
+                <li className="hover:border-b-2 border-b-purple-700">PortFolio</li>
+                <li className="hover:border-b-2 border-b-purple-700">Contact</li>
+                <li className="hover:border-b-2 border-b-purple-700">Blog</li>
+            </ul>
+
+            <div className="">
+                {/* dark/light  */}
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    {/* half circle  */}
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4c4.97 0 9 4.03 9 9s-4.03 9-9 9a9 9 0 01-9-9 9 9 0 019-9z" />
+                        
+                    {/* sun  */}
+                </svg>
+            </div>
+        </div>
         
     )
 }
