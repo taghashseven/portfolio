4cb158a1d9e98e3eb4b990c7895f1f317ad12a1d
jonah <mudzingwajona@gmail.com>
2023-10-03 22:31:32 +0200

    typing effect animation

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,18 +1,25 @@
 import './homePage.css'
-import {EnvelopIcons} from '@heroicons/react/outline'
-    
+import {EnvelopeIcon}   from '@heroicons/react/24/solid'
 
 const HomePage = ()=>{
     return (
         <div className="py-20 md:py-52 card relative  overflow-hidden">
             <img src='assets/me.png' alt='me' className='absolute top-0 z-[-1] imgme' />
 
-            <div className="card float-right mx-2 md:w-3/6 bg-gray-700/30 p-2 rounded-sm">
+            <div className="card float-right mx-2 md:w-3/6 bg-gray-700/30 p-2 rounded-sm ">
                 <span className="text-gray-400">Hello,</span>
                 <p className="text-2xl text-gray-200">I'm <span className="text-yellow-500 uppercase italic">Jonah</span></p>
-                <p className="text-3xl text-gray-100">Web Developer</p>
+                <div className="title">
+                    <p className="text-3xl text-gray-100">Web Developer</p>
+                    <p className="text-3xl text-gray-100">Freelancer</p>
+                    <p className="text-3xl text-gray-100">Network engineer</p>
+                    <p className="text-3xl text-gray-100">Electronics Engineer</p>
+                </div>
                 <p className="text-slate-500 ">"Crafting Web Experiences with Precision and Creativity"</p>
-                <button className="bg-slate-300 rounded p-1 mt-2"> email </button>
+                <button className="bg-slate-300 rounded p-1 mt-2 flex "> 
+                    <EnvelopeIcon className='w-6 h-6 fill-gray-700 mr-2' />
+                    email 
+                </button>
             </div>
 
     
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,9 @@
 
+import {SunIcon}   from '@heroicons/react/24/solid'
+import {MoonIcon}   from '@heroicons/react/24/solid'
+import {Bars3Icon}   from '@heroicons/react/24/solid'
+
+
 const NavBar = ()=>{
     return (
         <div className="z-5 text-white flex justify-between p-2 border-b-[1px] border-slate-100 sticky top-0 bg-slate-600/70  ">
@@ -6,12 +11,6 @@ const NavBar = ()=>{
 
             {/* if less than md */}
 
-            <div className="md:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    {/* three lines  */}
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" />
-                </svg>
-            </div>
 
             <ul className="flex justify-between gap-4 max-md:hidden">
                 <li className="hover:border-b-2 border-b-purple-700">Home</li>
@@ -21,15 +20,12 @@ const NavBar = ()=>{
                 <li className="hover:border-b-2 border-b-purple-700">Blog</li>
             </ul>
 
-            <div className="max-md:hidden">
-                {/* dark/light  */}
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    {/* half circle  */}
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4c4.97 0 9 4.03 9 9s-4.03 9-9 9a9 9 0 01-9-9 9 9 0 019-9z" />
+            <Bars3Icon className='md:hidden w-6 h-6 '/>
 
-                    {/* sun  */}
-                </svg>
-            </div>
+
+            <SunIcon  className='w-6 h-6 '/>
+            <MoonIcon  className='max-md:hidden w-6 h-6 hidden'/>
+            
         </div>
         
     )
